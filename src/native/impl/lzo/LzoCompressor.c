9b352cb2b7074e2a7463e4f5e066c8f0cebfc216
cnauroth <cnauroth@hotmail.com>
2013-09-03 16:56:30 -0700

    code compiling on Windows now, one TODO remaining

--- src/native/impl/lzo/LzoCompressor.c
+++ src/native/impl/lzo/LzoCompressor.c
@@ -22,8 +22,12 @@
 
 // The lzo2 library-handle
 static void *liblzo2 = NULL;
+// type of pointer to lzo function that returns version number
+typedef unsigned (__LZO_CDECL *lzo_version_t)();
 // lzo2 library version
 static jint liblzo2_version = 0;
+// type of pointer to lzo initialization function
+typedef int (__LZO_CDECL *lzo_init_t) (unsigned,int,int,int,int,int,int,int,int,int);
 
 // The lzo 'compressors'
 typedef struct {
@@ -34,6 +38,7 @@ typedef struct {
 } lzo_compressor;
 
 #define UNDEFINED_COMPRESSION_LEVEL -999
+#define MSG_LEN 32
 
 // Default compression level used when user supplies no value.
 static lzo_compressor lzo_compressors[] = {
@@ -121,6 +126,9 @@ JNIEXPORT void JNICALL
 Java_com_hadoop_compression_lzo_LzoCompressor_initIDs(
 	JNIEnv *env, jclass class
 	) {
+  void* lzo_version_ptr = NULL;
+
+#ifdef UNIX
 	// Load liblzo2.so
 	liblzo2 = dlopen(HADOOP_LZO_LIBRARY, RTLD_LAZY | RTLD_GLOBAL);
 	if (!liblzo2) {
@@ -129,6 +137,15 @@ Java_com_hadoop_compression_lzo_LzoCompressor_initIDs(
 	  THROW(env, "java/lang/UnsatisfiedLinkError", msg);
 	  return;
 	}
+#endif
+
+#ifdef WINDOWS
+  liblzo2 = LoadLibrary(HADOOP_LZO_LIBRARY);
+  if (!liblzo2) {
+    THROW(env, "java/lang/UnsatisfiedLinkError", "Cannot load lzo2.dll");
+    return;
+  }
+#endif
     
   LzoCompressor_clazz = (*env)->GetStaticFieldID(env, class, "clazz", 
                                                  "Ljava/lang/Class;");
@@ -155,27 +172,43 @@ Java_com_hadoop_compression_lzo_LzoCompressor_initIDs(
                                               "Ljava/nio/ByteBuffer;");
 
   // record lzo library version
-  void* lzo_version_ptr = NULL;
+#ifdef UNIX
   LOAD_DYNAMIC_SYMBOL(lzo_version_ptr, env, liblzo2, "lzo_version");
+#endif
+
+#ifdef WINDOWS
+  LOAD_DYNAMIC_SYMBOL(lzo_version_t, lzo_version_ptr, env, liblzo2, "lzo2_version");
+#endif
+
   liblzo2_version = (NULL == lzo_version_ptr) ? 0
-    : (jint) ((unsigned (__LZO_CDECL *)())lzo_version_ptr)();
+    : (jint) ((lzo_version_t)lzo_version_ptr)();
 }
 
 JNIEXPORT void JNICALL
 Java_com_hadoop_compression_lzo_LzoCompressor_init(
   JNIEnv *env, jobject this, jint compressor 
   ) {
+  void *lzo_init_func_ptr = NULL;
+  lzo_init_t lzo_init_function = NULL;
+  void *compressor_func_ptr = NULL;
+  int rv = 0;
   const char *lzo_compressor_function = lzo_compressors[compressor].function;
  
   // Locate the requisite symbols from liblzo2.so
-  dlerror();                                 // Clear any existing error
 
   // Initialize the lzo library 
-  void *lzo_init_func_ptr = NULL;
-  typedef int (__LZO_CDECL *lzo_init_t) (unsigned,int,int,int,int,int,int,int,int,int);
+
+#ifdef UNIX
+  dlerror();                                 // Clear any existing error
   LOAD_DYNAMIC_SYMBOL(lzo_init_func_ptr, env, liblzo2, "__lzo_init_v2");
-  lzo_init_t lzo_init_function = (lzo_init_t)(lzo_init_func_ptr);
-  int rv = lzo_init_function(LZO_VERSION, (int)sizeof(short), (int)sizeof(int), 
+#endif
+
+#ifdef WINDOWS
+  LOAD_DYNAMIC_SYMBOL(lzo_init_t, lzo_init_func_ptr, env, liblzo2, "__lzo_init_v2");
+#endif
+
+  lzo_init_function = (lzo_init_t)(lzo_init_func_ptr);
+  rv = lzo_init_function(LZO_VERSION, (int)sizeof(short), (int)sizeof(int), 
               (int)sizeof(long), (int)sizeof(lzo_uint32), (int)sizeof(lzo_uint), 
               (int)lzo_sizeof_dict_t, (int)sizeof(char*), (int)sizeof(lzo_voidp),
               (int)sizeof(lzo_callback_t));
@@ -185,8 +218,15 @@ Java_com_hadoop_compression_lzo_LzoCompressor_init(
   }
   
   // Save the compressor-function into LzoCompressor_lzoCompressor
-  void *compressor_func_ptr = NULL;
+
+#ifdef UNIX
   LOAD_DYNAMIC_SYMBOL(compressor_func_ptr, env, liblzo2, lzo_compressor_function);
+#endif
+
+#ifdef WINDOWS
+  LOAD_DYNAMIC_SYMBOL(void *, compressor_func_ptr, env, liblzo2, lzo_compressor_function);
+#endif
+
   (*env)->SetLongField(env, this, LzoCompressor_lzoCompressor,
                        JLONG(compressor_func_ptr));
   
@@ -206,37 +246,51 @@ JNIEXPORT jint JNICALL
 Java_com_hadoop_compression_lzo_LzoCompressor_compressBytesDirect(
   JNIEnv *env, jobject this, jint compressor 
 	) {
+  jobject clazz = NULL;
+  jobject uncompressed_direct_buf = NULL;
+  lzo_uint uncompressed_direct_buf_len = 0;
+  jobject compressed_direct_buf = NULL;
+  lzo_uint compressed_direct_buf_len = 0;
+  int compression_level = UNDEFINED_COMPRESSION_LEVEL;
+  jobject working_memory_buf = NULL;
+  jlong lzo_compressor_funcptr = 0;
+  lzo_bytep uncompressed_bytes = NULL;
+  lzo_bytep compressed_bytes = NULL;
+  lzo_voidp workmem = NULL;
+  lzo_uint no_compressed_bytes = 0;
+  int rv = 0;
+  char exception_msg[MSG_LEN];
   const char *lzo_compressor_function = lzo_compressors[compressor].function;
 
 	// Get members of LzoCompressor
-    jobject clazz = (*env)->GetStaticObjectField(env, this, 
+    clazz = (*env)->GetStaticObjectField(env, this, 
                                                  LzoCompressor_clazz);
-	jobject uncompressed_direct_buf = (*env)->GetObjectField(env, this, 
+	uncompressed_direct_buf = (*env)->GetObjectField(env, this, 
 									                    LzoCompressor_uncompressedDirectBuf);
-	lzo_uint uncompressed_direct_buf_len = (*env)->GetIntField(env, this, 
+	uncompressed_direct_buf_len = (*env)->GetIntField(env, this, 
 									                  LzoCompressor_uncompressedDirectBufLen);
 
-	jobject compressed_direct_buf = (*env)->GetObjectField(env, this, 
+	compressed_direct_buf = (*env)->GetObjectField(env, this, 
 									                        LzoCompressor_compressedDirectBuf);
-	lzo_uint compressed_direct_buf_len = (*env)->GetIntField(env, this, 
+	compressed_direct_buf_len = (*env)->GetIntField(env, this, 
 									                            LzoCompressor_directBufferSize);
 
   // Prefer the user defined compression level.
-  int compression_level = (*env)->GetIntField(env, this,
+  compression_level = (*env)->GetIntField(env, this,
       LzoCompressor_lzoCompressionLevel);
   if (UNDEFINED_COMPRESSION_LEVEL == compression_level) {
     compression_level = lzo_compressors[compressor].compression_level;
   }
 
-	jobject working_memory_buf = (*env)->GetObjectField(env, this, 
+	working_memory_buf = (*env)->GetObjectField(env, this, 
 									                      LzoCompressor_workingMemoryBuf);
 
-  jlong lzo_compressor_funcptr = (*env)->GetLongField(env, this,
+  lzo_compressor_funcptr = (*env)->GetLongField(env, this,
                   LzoCompressor_lzoCompressor);
 
     // Get the input direct buffer
     LOCK_CLASS(env, clazz, "LzoCompressor");
-	lzo_bytep uncompressed_bytes = (*env)->GetDirectBufferAddress(env, 
+	uncompressed_bytes = (*env)->GetDirectBufferAddress(env, 
                                             uncompressed_direct_buf);
     UNLOCK_CLASS(env, clazz, "LzoCompressor");
     
@@ -246,7 +300,7 @@ Java_com_hadoop_compression_lzo_LzoCompressor_compressBytesDirect(
 	
     // Get the output direct buffer
     LOCK_CLASS(env, clazz, "LzoCompressor");
-	lzo_bytep compressed_bytes = (*env)->GetDirectBufferAddress(env, 
+	compressed_bytes = (*env)->GetDirectBufferAddress(env, 
                                             compressed_direct_buf);
     UNLOCK_CLASS(env, clazz, "LzoCompressor");
     
@@ -256,7 +310,7 @@ Java_com_hadoop_compression_lzo_LzoCompressor_compressBytesDirect(
 	
     // Get the working-memory direct buffer
     LOCK_CLASS(env, clazz, "LzoCompressor");
-    lzo_voidp workmem = (*env)->GetDirectBufferAddress(env, working_memory_buf);
+    workmem = (*env)->GetDirectBufferAddress(env, working_memory_buf);
     UNLOCK_CLASS(env, clazz, "LzoCompressor");
     
   if (workmem == 0) {
@@ -264,8 +318,8 @@ Java_com_hadoop_compression_lzo_LzoCompressor_compressBytesDirect(
   }
   
 	// Compress
-  lzo_uint no_compressed_bytes = compressed_direct_buf_len;
-	int rv = 0;
+  no_compressed_bytes = compressed_direct_buf_len;
+	rv = 0;
   if (compression_level == UNDEFINED_COMPRESSION_LEVEL) {
     lzo_compress_t fptr = (lzo_compress_t) FUNC_PTR(lzo_compressor_funcptr);
     rv = fptr(uncompressed_bytes, uncompressed_direct_buf_len,
@@ -273,10 +327,12 @@ Java_com_hadoop_compression_lzo_LzoCompressor_compressBytesDirect(
               workmem);
   } else if (strstr(lzo_compressor_function, "lzo1x_999")
              || strstr(lzo_compressor_function, "lzo1y_999")) {
+/* TODO
     // Compression levels are only available in these codecs.
     rv = lzo1x_999_compress_level(uncompressed_bytes, uncompressed_direct_buf_len,
                                   compressed_bytes, &no_compressed_bytes,
                                   workmem, NULL, 0, 0, compression_level);
+*/
   } else {
     lzo_compress2_t fptr = (lzo_compress2_t) FUNC_PTR(lzo_compressor_funcptr);
     rv = fptr(uncompressed_bytes, uncompressed_direct_buf_len,
@@ -289,9 +345,14 @@ Java_com_hadoop_compression_lzo_LzoCompressor_compressBytesDirect(
     (*env)->SetIntField(env, this, 
                 LzoCompressor_uncompressedDirectBufLen, 0);
   } else {
-    const int msg_len = 32;
-    char exception_msg[msg_len];
-    snprintf(exception_msg, msg_len, "%s returned: %d", lzo_compressor_function, rv);
+#ifdef UNIX
+    snprintf(exception_msg, MSG_LEN, "%s returned: %d", lzo_compressor_function, rv);
+#endif
+
+#ifdef WINDOWS
+    _snprintf_s(exception_msg, MSG_LEN, _TRUNCATE, "%s returned: %d", lzo_compressor_function, rv);
+#endif
+
     THROW(env, "java/lang/InternalError", exception_msg);
   }
 
